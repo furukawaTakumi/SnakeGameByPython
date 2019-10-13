8f7f9c74a748882ce80401757b929c375e6e2734
furukawaTakumi <black3moon88@gmail.com>
2019-10-13 21:24:30 +0900

    App.pyを追加

---
+++ App.py
@@ -0,0 +1,15 @@
+import pyxel
+
+class App:
+    def __init__(self):
+        pyxel.init(160, 120)
+        pyxel.run(self.update, self.draw)
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+App()
