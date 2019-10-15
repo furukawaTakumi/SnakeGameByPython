cc1764e449b2c4810508d71021f2cdf92cd33cd8
furukawaTakumi <black3moon88@gmail.com>
2019-10-15 19:31:34 +0900

    スクリーンの表示にスコアを追加したStartScreenクラスの実装

---
+++ screen/StartScreen.py
@@ -0,0 +1,26 @@
+import pyxel
+from .Screen import Screen
+from .Struct import TextStruct, ButtonStruct
+from .Score import Score
+
+class StartScreen(Screen):
+    def __init__(self, title, button):
+        super().__init__(title, button)
+        self.score = Score()
+        pass
+
+    def ScreenUpdate(self):
+        super().ScreenUpdate()
+        first = TextStruct( "1st : " +  str(self.score.score_list[0]), 7 )
+        second = TextStruct( "2nd : " + str(self.score.score_list[1]), 7 )
+        third = TextStruct( "3rd : " + str(self.score.score_list[2]), 7 )
+        pos = self.title_pos
+        new_pos = list()
+        new_pos.append( pyxel.width//2 - first.size[0] // 2 )
+        new_pos.append( pos[1] + 15 )
+        pyxel.text( new_pos[0], new_pos[1], first.name, first.col )
+        new_pos[1] += 9
+        pyxel.text( new_pos[0], new_pos[1], second.name, second.col )
+        new_pos[1] += 9
+        pyxel.text( new_pos[0], new_pos[1], third.name, third.col )
+        pass
