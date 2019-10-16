4acacab6487695d8af54b9f4588f2a480ffb86cc
furukawaTakumi <black3moon88@gmail.com>
2019-10-16 09:56:06 +0900

    状態に応じたゲームの進行をできるようにした

--- App.py
+++ App.py
@@ -1,12 +1,12 @@
 import pyxel
-from screen.Screen import Screen
 from screen.ScreenCreator import CreateStartScreen
+from GameStatus import GameStatus
 
 class App:
     def __init__(self):
-        pyxel.init(160, 120)
+        pyxel.init(130, 90)
         pyxel.mouse(True)
-
+        self.now_status = GameStatus.START
         self.startScreen = CreateStartScreen()
         pyxel.run(self.update, self.draw)
         pass
@@ -16,7 +16,16 @@ class App:
 
     def draw(self):
         pyxel.cls(0)
-        self.startScreen.ScreenUpdate()
+        if self.now_status == GameStatus.START:
+            self.startScreen.ScreenUpdate()
+            if self.startScreen.isBtnClicked():
+                self.now_status = GameStatus.GAME
+
+        elif self.now_status == GameStatus.GAME:
+            pass
+        elif self.now_status == GameStatus.GAMEOVER:
+            pass
+
         pass
 
 App()
