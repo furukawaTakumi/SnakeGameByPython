89df9da12ff93c859e2fa84d1851afd62be4e794
furukawaTakumi <black3moon88@gmail.com>
2019-10-30 09:42:26 +0900

    CreateFeedメソッドを作った（未テスト）。でもやっぱり餌クラスを作ったほうが良さそう。

--- game_contents/GameController.py
+++ game_contents/GameController.py
@@ -1,4 +1,6 @@
 
+from random import sample
+
 from .SpecReader import SpecReader
 from .Snake import Snake
 from .Field import Field
@@ -17,5 +19,21 @@ class GameController():
         }
         self.__snake = Snake(snake_pos, snake_vec)
 
-        self.__field_size = ( reader.spec["fieldrow"], reader.spec["fieldcolmun"] )
-        self.__field = Field(self.__field_size)
+        field_size = ( reader.spec["fieldrow"], reader.spec["fieldcolmun"] )
+        self.__field = Field(field_size)
+
+    def CreateFeed(self):
+        row_pos = len( self.__field.block_list )
+        column_pos = len( self.__field.block_list[0] )
+
+        # 適切な位置が帰ってくるまでサイコロを振り続ける
+        create_pos_x = 0
+        create_pos_y = 0
+        while True:
+            create_pos_x = sample( range(1, column_pos-1), 1 )[0] # 長さ1のリストを生成する
+            create_pos_y = sample( range(1, row_pos-1), 1 )[0]
+
+            if pState.NONE == self.__field.block_list[create_pos_x][create_pos_y]:
+                break
+
+        self.__field.block_list[create_pos_x][create_pos_y] = pState.FEED
