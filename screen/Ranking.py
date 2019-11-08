6924ec412dae74530181b25913c4ba5ae44119c7
furukawaTakumi <black3moon88@gmail.com>
2019-11-08 09:07:48 +0900

    ScoreBoardクラスをRankingクラスに変更した

--- screen/Ranking.py
+++ screen/Ranking.py
@@ -1,9 +1,6 @@
 
-class ScoreBoard:
-    def __init__(self):
-        # スタート画面に表示するスコアリスト生成
-        self.__score_list = []
-
+class Ranking:
+    @staticmethod
     def Top3(self, filepath="asset/score.txt"):
         self.__score_list = []
         with open(filepath, mode='r') as file:
@@ -11,6 +8,7 @@ class ScoreBoard:
                 self.__score_list.append( int( file.readline() ) )
         return self.__score_list
 
+    @staticmethod
     def RankedRecord(self, filepath="asset/score.txt"):
         with open(filepath, "r") as file:
             scores = file.readlines()
@@ -24,6 +22,7 @@ class ScoreBoard:
                 rank -= 1
         return rank
 
+    @staticmethod
     def SortScoreAndReWrite(self, filepath="asset/score.txt"):
         with open(filepath, "r") as file:
             scores = file.readlines()
@@ -34,8 +33,3 @@ class ScoreBoard:
 
         with open(filepath, "w") as file:
             file.writelines( "\n".join(scores) )
-
-
-    @property
-    def score_list(self):
-        return self.__score_list
