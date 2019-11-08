d71be8e3792aa1e14c5c134c4c29791c1268b99e
furukawaTakumi <black3moon88@gmail.com>
2019-11-08 10:03:57 +0900

    PrepareDisplayメソッドにリネーム

--- screen/GameOverScreen.py
+++ screen/GameOverScreen.py
@@ -3,7 +3,8 @@ import pyxel
 from copy import deepcopy
 from .Screen import Screen
 from .Struct import TextStruct
-from .ScoreBoard import ScoreBoard
+from .Ranking import Ranking
+from .Celebratory import Celebratory
 
 
 class GameOverScreen(Screen):
@@ -11,13 +12,15 @@ class GameOverScreen(Screen):
         super().__init__(title, button)
         self.scoreboard = ScoreBoard()
         self.before_score_list = deepcopy( self.scoreboard.score_list )
-        self.celebrate_txt = ["You are First!!", "You are Second!!", "You are third!!", ""]
         pass
+
+    def PrepareDisplay(self):
+        self.score_rank = Ranking.RankedRecord()
+        Ranking.SortScoreAndReWrite()
 
     def ScreenUpdate(self, score):
         super().ScreenUpdate()
-        idx = self.calcDiffScoreIdx(score)
-        celebrate_txt = TextStruct(self.celebrate_txt[idx], 10)
-        myrecord = TextStruct( "record: " + str( score ), 7 )
+        celebrate_txt = TextStruct( Celebratory.CreateCelebratory(self.score_rank), 10)
+        myrecord = TextStruct( "record: " + self.score_rank, 7 )
         pyxel.text( pyxel.width//2 - myrecord.size[0] // 2, self.title_pos[1]+15, myrecord.name, myrecord.col )
         pyxel.text( pyxel.width//2 - celebrate_txt.size[0] // 2, self.title_pos[1]+25, celebrate_txt.name, celebrate_txt.col )
